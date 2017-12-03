c77b4c6ba09297d0facb2c5a84ba6a03547463bd
Stephen Wicks <stevewicks1@gmail.com>
2017-12-02 23:23:29 -0500

    c

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
+import '../Dashboard/d';
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
-        <div className="row">
-          <div className="col s12 align-center">
-            <h3>Dashboard</h3>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col s12">
-            buttons go here...
-          </div>
-        </div>
+       <div className="header">
+     {/* <h1><img src="logo-express.png" alt="" height="102" width="300"></h1>  */}
+        </div> 
+    <nav>
+      <ul></ul>
+    </nav>
+    <div className="fullWidthDiv">
+      <div className="headerText">
+        <h1>Your Business<br/>SIMPLIFIED</h1>
+     </div>
+    </div>
+  
+   
+   <div className="leftSideMenu">
+     <ul>
+       <li>Home</li>
+       <li>Create An Account</li>
+       <li>View Accounts</li>
+       <li>Log Out</li>
+     </ul>
+   </div> 
+   
+   <div className="main">
+     <h1>Your Account</h1>
+     <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+     <p>Resize the browser window to see how the content respond to the resizing.</p>
+   </div>
       </div>
     )
   }
